e890b0185a7160886382f5be3a395cdf150d6638
jack-njenga <jacknjenga99@gmail.com>
2023-05-08 12:55:19 -0700

    update

---
+++ 0x00-python-hello_world/100-write.py
@@ -0,0 +1,5 @@
+import sys
+
+messege = "and that piece of art is useful - Dora Korpar, 2015-10-19\n"
+sys.stderr.write(messege)
+sys.exit(1)
---
+++ 0x00-python-hello_world/102-magic_calculation.py
@@ -0,0 +1,3 @@
+def magic_calculation(a, b):
+    c = b ** 2
+    return (a + c + 98)
