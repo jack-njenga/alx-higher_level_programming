96a399ca6ac16c48c2024b3793bc54a167faf5c3
jack-njenga <jacknjenga99@gmail.com>
2023-05-09 06:13:56 -0700

    _update

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,27 +1,20 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
+rem = str(number)[-1]
+str1 = "Last digit of"
+str2 = "and is less than 6 and not 0"
 if number < 0:
-    rem = number * -1
-    while (rem > 10):
-        rem = rem % 10
-        if rem == 0:
-            print("Last digit of", number, "is", rem, "and is 0")
-        elif rem < 10:
-            _rem = "-" + str(rem)
-            print("Last digit of", number, "is", _rem, "and is less than 6 and not 0")
-        else:
-            continue
+    if rem == "0":
+        print(str1, number, "is", rem, "and is 0")
+    else:
+        _rem = "-" + rem
+        print(str1, number, "is", _rem, str2)
 else:
-    rem = number
-    while (rem > 10):
-        rem = rem % 10
-        if rem == 0:
-            print("Last digit of", number, "is", rem, "and is 0")
-        elif rem < 10:
-            if rem < 6:
-                print("Last digit of", number, "is", rem, "and is less than 6 and not 0")
-            elif rem > 5:
-                print("Last digit of", number, "is", rem, "and is greater than 5")
-        else:
-            continue
+    rem = int(rem)
+    if (rem < 6 and rem > 0):
+        print(str1, number, "is", rem, str2)
+    elif (rem == 0):
+        print(str1, number, "is", rem, "and is 0")
+    else:
+        print(str1, number, "is", rem, "and is greater than 5")
