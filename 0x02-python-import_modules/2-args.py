3dfc04f53cbc45c94ed29bc942dd294fd98f519f
jack-njenga <jacknjenga99@gmail.com>
2023-05-11 21:35:33 +0300

    who are you

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -4,13 +4,13 @@ if __name__ == "__main__":
     import sys
 
     argv = sys.argv
-    l = len(argv) - 1
-    if l == 0:
+    ln = len(argv) - 1
+    if ln == 0:
         print("0 arguments.")
         exit(0)
-    if l == 1:
+    if ln == 1:
         print("{} argument:".format(1))
     else:
-        print("{} arguments:".format(l))
+        print("{} arguments:".format(ln))
     for i in range(1, len(argv)):
         print("{}: {}".format(i, argv[i]))
