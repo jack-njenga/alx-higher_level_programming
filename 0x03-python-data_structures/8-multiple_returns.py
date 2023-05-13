8ac7e3e366466e934b36a99053f9de8931de6469
jack njenga <jacknjenga99@gmail.com>
2023-05-13 12:13:38 -0700

    update

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -3,6 +3,5 @@
 def multiple_returns(sentence):
     if len(sentence) == 0:
         return (0, None)
-
-    ret = (len(sentence), sentence[0])
-    return ret
+    else:
+        return (len(sentence), sentence[0])
