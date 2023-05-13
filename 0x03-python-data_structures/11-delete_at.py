a5cbc1b6623554877f133fad47b0cfbba2e9055b
jack njenga <jacknjenga99@gmail.com>
2023-05-13 12:01:15 -0700

    update

--- 0x03-python-data_structures/11-delete_at.py
+++ 0x03-python-data_structures/11-delete_at.py
@@ -1,8 +1,11 @@
 #!/usr/bin/python3
 
 def delete_at(my_list=[], idx=0):
-    if (my_list is None):
-        return None
-
-    del my_list[idx]
+    new = []
+    for i in range(len(my_list)):
+        if i == idx:
+            pass
+        else:
+            new.append(my_list[i])
+    my_list[:] = new
     return (my_list)
--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,8 +1,8 @@
 #!/usr/python3
 
 def multiple_returns(sentence):
-    if sentence is None:
-        return tuple([0, 'None'])
+    if len(sentence) == 0:
+        return tuple([0, None])
 
     ret = [len(sentence), sentence[0]]
     return tuple(ret)
