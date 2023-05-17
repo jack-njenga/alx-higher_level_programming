b235525a5ebae7d9d79d059dc226bfeff782bc7a
jack njenga <jacknjenga99@gmail.com>
2023-05-18 00:35:35 +0300

    3 more advance

--- 0x04-python-more_data_structures/101-square_matrix_map.py
+++ 0x04-python-more_data_structures/101-square_matrix_map.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
 def square_matrix_map(matrix=[]):
-    return (list(map(lambda _list: list(map(lambda num: num ** 2, _list)), matrix)))
+    return (list(map(lambda _list: list(map(lambda num: num ** 2, _list)), (matrix))))
