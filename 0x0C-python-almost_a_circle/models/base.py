0f9f9a99a83de7138d048c4bd637f3b672beeb54
jack-njenga <jacknjenga99@gmail.com>
2023-06-17 14:10:14 +0300

    update on base class

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -23,3 +23,29 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    def to_json_string(list_dictionaries):
+        """
+        """
+        import json
+
+        if list_dictionaries == None:
+            return "[]"
+        else:
+            js_list = ""
+            for dic in list_dictionaries:
+                js_list = js_list + json.dumps(dic)
+
+            return "[" + js_list + "]"
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        """
+        """
+        filename = cls.__name__ + ".json"
+        obj = []
+        if list_objs is not None:
+            for i in list_objs:
+                obj.append(cls.to_dictionary(i))
+            with open(filename, "w", encoding="utf-8") as f:
+                f.write(cls.to_json_string(obj))
