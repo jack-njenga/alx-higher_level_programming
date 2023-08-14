175b452d59b3a26ed8f8c9b05c3a3db2b301efd2
jack-njenga <jacknjenga99@gmail.com>
2023-08-14 17:58:50 +0300

    First constant, first print

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,8 +1,7 @@
 #!/usr/bin/node
-let line1, line2, line3;
-line1 = "C is fun";
-line2 = "Python is cool";
-line3 = "JavaScript is amazing";
+const line1 = "C is fun";
+const line2 = "Python is cool";
+const line3 = "JavaScript is amazing";
 
 console.log(line1);
 console.log(line2);
