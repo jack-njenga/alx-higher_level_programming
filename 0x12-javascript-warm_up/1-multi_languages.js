db739680ed812d49f3647f9e94f7302f62f8008f
jack-njenga <jacknjenga99@gmail.com>
2023-08-14 17:55:12 +0300

    First constant, first print

---
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+let line1, line2, line3;
+line1 = "C is fun";
+line2 = "Python is cool";
+line3 = "JavaScript is amazing";
+
+console.log(line1);
+console.log(line2);
+console.log(line3);
