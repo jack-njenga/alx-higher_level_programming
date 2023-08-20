56173e8d065c8cbad6c591c0de078a35e0c6850f
jack-njenga <jacknjenga99@gmail.com>
2023-08-20 17:41:34 +0300

    states filter

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,11 +1,15 @@
 #!/usr/bin/python3
 """
+lists all states with a name starting with N
+(upper N) from the database hbtn_0e_0_usa.
 """
 
 
 def list_n_states(username, passwd, d_b):
     import MySQLdb as sqldb
     """
+    lists all states with a name starting with N
+    (upper N) from the database hbtn_0e_0_usa.
     """
     try:
         db_config = {
@@ -19,24 +23,22 @@ def list_n_states(username, passwd, d_b):
         cursor = db.cursor()
 
         query = "SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id"
-        cursor.excute(query)
+        cursor.execute(query)
 
         states = cursor.fetchall()
         for state in states:
-            print("=> ", state)
+            print(state)
 
     except sqldb.Error as e:
         print(e)
     if db:
         db.close()
 
+
 if __name__ == "__main__":
     import sys
     if len(sys.argv) >= 4:
-        print("yes")
         name = sys.argv[1]
         passwd = sys.argv[2]
         d_b = sys.argv[3]
         list_n_states(name, passwd, d_b)
-    else:
-        print("No")
