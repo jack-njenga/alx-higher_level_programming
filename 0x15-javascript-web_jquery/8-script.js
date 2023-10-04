ef725d1b12d09f4faddf9dc6cd0afb6dedb645ac
jack-njenga <jacknjenga99@gmail.com>
2023-10-04 18:51:58 +0300

    all mandatory

---
+++ 0x15-javascript-web_jquery/8-script.js
@@ -0,0 +1,8 @@
+$.get('https://swapi-api.alx-tools.com/api/films/?format=json', function (data, status) {
+  const movies = data.results;
+  for (let i = 0; i < movies.length; i++) {
+    const title = $('<li></li>').text(movies[i].title);
+    $('UL#list_movies').append(title);
+  }
+  // $("UL#list_movies").append(...data.results.map(movie => `<li>${movie.title}</li>`));
+});
